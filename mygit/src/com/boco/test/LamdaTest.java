654f5dede9555c089c83aa59f561656a164c2b89
zhaozhihua2009 <zhaozhihua2008@126.com>
2017-06-29 20:03:43 +0800

    bc-2

--- mygit/src/com/boco/test/LamdaTest.java
+++ mygit/src/com/boco/test/LamdaTest.java
@@ -3,6 +3,6 @@ package com.boco.test;
 public class LamdaTest {
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("bc-1");
+		System.out.println("bc-2");
 	}
 }
