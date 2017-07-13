b2f10af2667e83ff12a3d4b16502b29e6a90e578
zhaozhihua <zhaozhihua@boco.com.cn>
2017-07-13 14:49:30 +0800

    merge-3-4

--- mygit/src/com/boco/test/LamdaTest.java
+++ mygit/src/com/boco/test/LamdaTest.java
@@ -4,6 +4,7 @@ public class LamdaTest {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("bc-2");
+		System.out.println("bc-3");
 		System.out.println("bc-4");
 	}
 }
